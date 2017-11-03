23377d25ed512c76d8823bd3105de8a8592dafd8
STUDENTS\1795827 <azam.shirani@gmail.com>
2017-11-03 09:35:20 -0400

    no message

---
+++ cloudstorage.php
@@ -0,0 +1,41 @@
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+session_start();
+
+require_once 'vendor/autoload.php';
+
+DB::$dbName = 'cp4809_storage';
+DB::$user = 'cp4809_storage';
+DB::$encoding = 'utf8';
+
+DB::$password = 'OzrAns;t}tL4';
+// Slim creation and setup
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+        ));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => dirname(__FILE__) . '/cache'
+);
+$view->setTemplatesDirectory(dirname(__FILE__) . '/templates');
+
+// create a log channel
+$log = new Logger('main');
+$log->pushHandler(new StreamHandler('logs/everything.log', Logger::DEBUG));
+$log->pushHandler(new StreamHandler('logs/errors.log', Logger::ERROR));
+
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['user'] = array();
+}
+// url/event handler go here
+$app->get('/', function() use ($app) {
+    echo "This is stprage project";
+});
+
+$app->run();

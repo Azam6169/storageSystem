2ac4aadccd21419df930e78a80d6a324baa7a520
STUDENTS\1795827 <azam.shirani@gmail.com>
2017-11-03 13:33:13 -0400

    *Securename for folder

--- cloudstorage.php
+++ cloudstorage.php
@@ -37,5 +37,36 @@ if (!isset($_SESSION['user'])) {
 $app->get('/', function() use ($app) {
     echo "This is storage project";
 });
+//--------------------------------------------------GET LOGIN
+$app->get('/login', function() use ($app) {
+    $app->render('login.html.twig');
+});
+//--------------------------------------------------POST LOGIN
+$app->post('/login',function() use ($app) {
+    $email = $app->request()->post('email');
+    $pass = $app->request()->post('password');
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $error = true;
+    if ($row) {
+        if(password_verify($pass, $row['password'])){
+        //if ($pass == $row['password']) {
+            $error = false;
+        }
+    }
+    if ($error) {
+        $app->render('login.html.twig', array('error' => true));
+    } else {
+        unset($row['password']);
+        $_SESSION['user'] = $row;
+        $app->render('login_success.html.twig', array('userSession' => $_SESSION['user']));
+    }    
+});
+
+//--------------------------------------------------GET LOGOUT
+$app->get('/logout', function() use ($app) {
+    $_SESSION['user'] = array();
+    $app->render('logout.html.twig', array('userSession' => $_SESSION['user']));
+});
+
 
 $app->run();

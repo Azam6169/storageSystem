3ae4b3fea5de399bea115ecaa1e2df0dedd2fab1
STUDENTS\1795827 <azam.shirani@gmail.com>
2017-11-06 11:39:58 -0500

    *Function in storage file

--- cloudstorage.php
+++ cloudstorage.php
@@ -37,20 +37,35 @@ if (!isset($_SESSION['user'])) {
 $app->get('/', function() use ($app) {
     echo "This is storage project";
 });
-//--------------------------------------------------GET LOGIN
+// create a log channel
+
+$log = new Logger('mail');
+$log->pushHandler(new StreamHandler('logs/everything.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('logs/error.log', Logger::WARNING));
+  
+$twig = $app->view()->getEnvironment();
+$twig->addGlobal('usersession',$_SESSION['user']); 
+if(!isset($_SESSION['user'])){    
+    $_SESSION['user']=array();
+}
+$app->get('/logout', function() use ($app) {
+    $_SESSION['user'] = array();
+    $app->render('logout.html.twig');
+});
 $app->get('/login', function() use ($app) {
     $app->render('login.html.twig');
 });
-//--------------------------------------------------POST LOGIN
-$app->post('/login',function() use ($app) {
-    $email = $app->request()->post('email');
-    $pass = $app->request()->post('password');
-    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
-    $error = true;
-    if ($row) {
-        if(password_verify($pass, $row['password'])){
-        //if ($pass == $row['password']) {
-            $error = false;
+$app->post('/login', function() use ($app) {
+    
+$email = $app->request()->post('email');
+$password = $app->request()->post('pass');
+$row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $error = false;
+    if (!$row) {
+        $error = true; // user not found
+    } else {
+        if (password_verify($password, $row['password'])== FALSE) {
+            $error = true; // password invalid
         }
     }
     if ($error) {
@@ -58,15 +73,264 @@ $app->post('/login',function() use ($app) {
     } else {
         unset($row['password']);
         $_SESSION['user'] = $row;
-        $app->render('login_success.html.twig', array('userSession' => $_SESSION['user']));
-    }    
+        $app->render('login_success.html.twig',array('userSession' => $_SESSION['user']));
+    }
 });
-
-//--------------------------------------------------GET LOGOUT
+$app->get('/', function() use ($app) {
+     $app->render('index.html.twig',array('userSession' => $_SESSION['user']));
+ 
+});
+/* * ****************** check email if registered *********************** */
+$app->get('/isemailregistered/:email', function($email)use($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="color:red; font-weight:bold;">Email already registered.</span>';
+});
+/* * ****************** check username if taken *********************** */
+$app->get('/isusernametaken/:username', function($name)use($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE name=%s", $name);
+    echo!$row ? "" : '<span style="color:red; font-weight:bold;">Username already taken.</span>';
+});
+$app->get('/isemailregistered/:email', function($email) use ($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="background-color: red; font-weight: bold;">Email already taken</span>';
+});
+$app->get('/register', function() use ($app) {
+    $app->render('register.html.twig');
+});
+$app->post('/register', function() use ($app) {
+    $name = $app->request()->post('name');
+    $email = $app->request()->post('email');
+    $pass1 = $app->request()->post('pass1');
+    $pass2 = $app->request()->post('pass2');
+    //
+    $values = array('name' => $name, 'email' => $email);
+    $errorList = array();
+    //
+    if (strlen($name) < 2 || strlen($name) > 50) {
+        $values['name'] = '';
+        array_push($errorList, "Name must be between 2 and 50 characters long");
+    }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+        $values['email'] = '';
+        array_push($errorList, "Email must look like a valid email");
+    } else {
+        $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+        if ($row) {
+            $values['email'] = '';
+            array_push($errorList, "Email already in use");
+        }
+    }
+    if ($pass1 != $pass2) {
+        array_push($errorList, "Passwords don't match");
+    } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
+        if (strlen($pass1) < 2 || strlen($pass1) > 50) {
+            array_push($errorList, "Password must be between 2 and 50 characters long");
+        }
+    }
+    //
+    if ($errorList) { // 3. failed submission
+        $app->render('register.html.twig', array(
+            'errorList' => $errorList,
+            'v' => $values));
+    } else { // 2. successful submission
+        $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
+        DB::insert('users', array('name' => $name, 'email' => $email, 'password' => $passEnc));
+        $app->render('register_success.html.twig');
+    }
+    
+});// create a log channel
+$log = new Logger('mail');
+$log->pushHandler(new StreamHandler('logs/everything.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('logs/error.log', Logger::WARNING));
+  
+$twig = $app->view()->getEnvironment();
+$twig->addGlobal('usersession',$_SESSION['user']); 
+if(!isset($_SESSION['user'])){    
+    $_SESSION['user']=array();
+}
 $app->get('/logout', function() use ($app) {
     $_SESSION['user'] = array();
-    $app->render('logout.html.twig', array('userSession' => $_SESSION['user']));
+    $app->render('logout.html.twig');
+});
+$app->get('/login', function() use ($app) {
+    $app->render('login.html.twig');
+});
+$app->post('/login', function() use ($app) {
+    
+$email = $app->request()->post('email');
+$password = $app->request()->post('pass');
+$row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    $error = false;
+    if (!$row) {
+        $error = true; // user not found
+    } else {
+        if (password_verify($password, $row['password'])== FALSE) {
+            $error = true; // password invalid
+        }
+    }
+    if ($error) {
+        $app->render('login.html.twig', array('error' => true));
+    } else {
+        unset($row['password']);
+        $_SESSION['user'] = $row;
+        $app->render('login_success.html.twig',array('userSession' => $_SESSION['user']));
+    }
+});
+$app->get('/', function() use ($app) {
+     $app->render('index.html.twig',array('userSession' => $_SESSION['user']));
+ 
+});
+/* * ****************** check email if registered *********************** */
+$app->get('/isemailregistered/:email', function($email)use($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="color:red; font-weight:bold;">Email already registered.</span>';
+});
+/* * ****************** check username if taken *********************** */
+$app->get('/isusernametaken/:username', function($name)use($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE name=%s", $name);
+    echo!$row ? "" : '<span style="color:red; font-weight:bold;">Username already taken.</span>';
+});
+$app->get('/isemailregistered/:email', function($email) use ($app) {
+    $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+    echo!$row ? "" : '<span style="background-color: red; font-weight: bold;">Email already taken</span>';
+});
+$app->get('/register', function() use ($app) {
+    $app->render('register.html.twig');
+});
+$app->post('/register', function() use ($app) {
+    $name = $app->request()->post('name');
+    $email = $app->request()->post('email');
+    $pass1 = $app->request()->post('pass1');
+    $pass2 = $app->request()->post('pass2');
+    //
+    $values = array('name' => $name, 'email' => $email);
+    $errorList = array();
+    //
+    if (strlen($name) < 2 || strlen($name) > 50) {
+        $values['name'] = '';
+        array_push($errorList, "Name must be between 2 and 50 characters long");
+    }
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) == FALSE) {
+        $values['email'] = '';
+        array_push($errorList, "Email must look like a valid email");
+    } else {
+        $row = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
+        if ($row) {
+            $values['email'] = '';
+            array_push($errorList, "Email already in use");
+        }
+    }
+    if ($pass1 != $pass2) {
+        array_push($errorList, "Passwords don't match");
+    } else { // TODO: do a better check for password quality (lower/upper/numbers/special)
+        if (strlen($pass1) < 2 || strlen($pass1) > 50) {
+            array_push($errorList, "Password must be between 2 and 50 characters long");
+        }
+    }
+    //
+    if ($errorList) { // 3. failed submission
+        $app->render('register.html.twig', array(
+            'errorList' => $errorList,
+            'v' => $values));
+    } else { // 2. successful submission
+        $passEnc = password_hash($pass1, PASSWORD_BCRYPT);
+        DB::insert('users', array('name' => $name, 'email' => $email, 'password' => $passEnc));
+        $app->render('register_success.html.twig');
+    }
+//////////////Upload File//////////////
+    $app->get('/share', function() use($app){
+
+
+    $app->render('share.html.twig');
+
 });
 
+$app->post('/share', function() use($app) {
+   });
+    
+ //Load the settings
+require_once("index.php");
+ 
+$message = "";
+//Has the user uploaded something?
+if(isset($_FILES['file']))
+{
+$target_path = Settings::$uploadFolder;
+$target_path = $target_path . time() . '_' . basename( $_FILES['file']['name']);
+}
+//Check the password to verify legal upload
+if($_POST['password'] != Settings::$password)
+{
+    $message = "Invalid Password!";
+}
+else
+{
+    //Try to move the uploaded file into the designated folder
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+            $message = "The file ".  basename( $_FILES['file']['name']). 
+            " has been uploaded";
+        } else{
+            $message = "There was an error uploading the file, please try again!";
+        }
+    }
+if(strlen($message) > 0)
+{
+    $message = '<p class="error">' . $message . '</p>';
+}
+//** LIST UPLOADED FILES **/
+$uploaded_files = "";
+ 
+//Open directory for reading
+$dh = opendir(Settings::$uploadFolder);
+//LOOP through the files
+while (($file = readdir($dh)) !== false) 
+{
+    if($file != '.' && $file != '..')
+{
+
+$filename = Settings::$uploadFolder . $file;
+$parts = explode("_", $file);
+$size = formatBytes(filesize($filename));
+$added = date("m/d/Y", $parts[0]);
+$originName = $parts[1];
+$filetype = getFileType(substr($file, strlen($file) - 3));
+$uploaded_files .= "<li class=\"$filetype\"><a href=\"$filename\">$origName</a> $size - $added</li>\n";
+}
+}
+//Secure file:
+$secretdir = md5(date("s-u"));
+echo $secretdir;
+closedir($dh);
+if(strlen($uploaded_files) == 0)
+{
+    $uploaded_files = "<li><em>No files found</em></li>";
+}
+function FormatSize($format){
+    $unit =array('B','KB','MB','GB','TB');
+    
+}
+function getFileType($extension)
+{
+    $images = array('jpg', 'gif', 'png', 'bmp');
+    $docs   = array('txt', 'rtf', 'doc');
+    $apps   = array('zip', 'rar', 'exe');
+     
+    if(in_array($extension, $images)) return "Images";
+    if(in_array($extension, $docs)) return "Documents";
+    if(in_array($extension, $apps)) return "Applications";
+    return "";
+}
+function formatBytes($bytes, $precision = 2) { 
+    $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+    
+    $bytes = max($bytes, 0); 
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+    $pow = min($pow, count($units) - 1); 
+    
+    $bytes /= pow(1024, $pow); 
+    
+    return round($bytes, $precision) . ' ' . $units[$pow]; 
+}
+});
 
 $app->run();

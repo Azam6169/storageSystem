122fbb6113e691cd0f7a798b3c101ec6e41f9470
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2017-11-11 11:22:40 -0500

    *Modify login

--- cloudstorage.php
+++ cloudstorage.php
@@ -133,6 +133,32 @@ $app->post('/share', function() use ($app, $log) {
     $values = array('filename' => $filename);
 
     $app->render('share.html.twig', array('v' => $values));
+    
+$container['upload_directory'] = '/uploads';
+
+$app->post('/', function(Request $request, Response $response) {
+    $directory = $this->get('upload_directory');
+
+    $uploadedFiles = $request->getUploadedFiles();
+
+    foreach ($uploadedFiles['example3'] as $uploadedFile) {
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $filename = moveUploadedFile($directory, $uploadedFile);
+            $response->write('uploaded ' . $filename . '<br/>');
+        }
+    }
+    
+function moveUploadedFile($directory, UploadedFile $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
+}
+
 });
 
 $app->run();

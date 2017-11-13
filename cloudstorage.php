85b7eb942744284b173bc8adba93af3a8e2b0bee
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2017-11-13 15:26:37 -0500

    *Resset password

--- cloudstorage.php
+++ cloudstorage.php
@@ -1,38 +1,13 @@
 <?php
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
-session_start();
-
-require_once 'vendor/autoload.php';
-
-DB::$dbName = 'cp4809_storage';
-DB::$user = 'cp4809_storage';
-DB::$encoding = 'utf8';
-
-DB::$password = 'OzrAns;t}tL4';
-// Slim creation and setup
-$app = new \Slim\Slim(array(
-    'view' => new \Slim\Views\Twig()
-        ));
-
-$view = $app->view();
-$view->parserOptions = array(
-    'debug' => true,
-    'cache' => dirname(__FILE__) . '/cache'
-);
-$view->setTemplatesDirectory(dirname(__FILE__) . '/templates');
-
-// create a log channel
-$log = new Logger('mail');
-$log->pushHandler(new StreamHandler('logs/everything.log', Logger::WARNING));
-$log->pushHandler(new StreamHandler('logs/error.log', Logger::WARNING));
-
-
-if (!isset($_SESSION['user'])) {
-    $_SESSION['user'] = array();
+if (false) {
+    $app = new \Slim\Slim();
+    $log = new Logger('main');
 }
+
+
+
 $app->get('/logout', function() use ($app) {
     $_SESSION['user'] = array();
     $app->render('logout.html.twig');
@@ -303,4 +278,4 @@ function moveUploadedFile($directory, UploadedFile $uploadedFile)
     return $filename;
 }
 
-$app->run();
+

336796a3fa01aed2ce5caa6b265165d8445adbf8
DESKTOP-U772PUB\Azam <azam.shirani@gmail.com>
2017-11-18 19:35:00 -0500

    *Share file *Daily scrum *Presentation

--- files.php
+++ files.php
@@ -19,20 +19,20 @@ $app->get('/list', function() use ($app) {
     }
     $userId = $_SESSION['user']['id'];
     $fileList = DB::query("SELECT * FROM files "
-            . "WHERE userid =%i", $userId); 
-    $sharefilelist =DB::query("SELECT F.* FROM files as F INNER JOIN shares as S ON F.id = S.fileid WHERE S.userid = %i", $userId);
-    
-    
-    $app->render("/file_list.html.twig", array('ownlist' => $fileList,'sharelist' =>$sharefilelist));
+                    . "WHERE userid =%i", $userId);
+    $sharefilelist = DB::query("SELECT F.* FROM files as F INNER JOIN shares as S ON F.id = S.fileid WHERE S.userid = %i", $userId);
+
+
+    $app->render("/file_list.html.twig", array('ownlist' => $fileList, 'sharelist' => $sharefilelist));
 });
 
 // Delete File 
 $app->get('/file/delete/:id', function($id) use ($app) {
-    if (!$_SESSION['user'] ) {
+    if (!$_SESSION['user']) {
         $app->render("access_denied.html.twig");
         return;
     }
-    $file = DB::queryFirstRow("SELECT * FROM files WHERE id=%d" , $id);
+    $file = DB::queryFirstRow("SELECT * FROM files WHERE id=%d", $id);
     if (!$file) {
         $app->render("/not_found.html.twig");
         return;
@@ -61,7 +61,7 @@ $app->post('/file/delete/:id', function($id) use ($app) {
 
 // Add-Edit File
 $app->get('/file/:op(/:id)', function($op, $id = -1) use ($app) {
-    if (!$_SESSION['user'] ){ 
+    if (!$_SESSION['user']) {
         $app->render('access_denied.html.twig');
         return;
     }
@@ -76,7 +76,7 @@ $app->get('/file/:op(/:id)', function($op, $id = -1) use ($app) {
             $app->render('/not_found.html.twig');
             return;
         }
-    } else { 
+    } else {
         $values = array();
     }
     $app->render('/file_addedit.html.twig', array(
@@ -97,14 +97,13 @@ $app->post('/file/:op(/:id)', function($op, $id = -1) use ($app, $log) {
         $app->render('/not_found.html.twig');
         return;
     }
-   
+
     $file = $_FILES['filename'];
     $filename = $file['name'];
     $values = array('filename' => $filename);
     $errorList = array();
-   
+
     //var_dump($file);
- 
     // is file being uploaded
 //    echo $_FILES['filename']['error'];
 //    print_r($_FILES);
@@ -114,7 +113,7 @@ $app->post('/file/:op(/:id)', function($op, $id = -1) use ($app, $log) {
             array_push($errorList, "Error uploading file");
             $log->err("Error uploading file: " . print_r($filename, true));
         } else {
-            if (strstr($filename , '..')) {
+            if (strstr($filename, '..')) {
                 array_push($errorList, "Invalid file name");
                 $log->warn("Uploaded file name with .. in it (possible attack): " . print_r($filename, true));
             }
@@ -132,36 +131,32 @@ $app->post('/file/:op(/:id)', function($op, $id = -1) use ($app, $log) {
         }
     } else { // no file uploaded
 //        echo 'Line 134';
-        
-            array_push($errorList, "file is required when creating new product");
-    
+        array_push($errorList, "Choose A File For Upload");
     }
-   
-    
+
+
     $ext = pathinfo($filename, PATHINFO_EXTENSION);
     //var_dump($ext);
     //die();
-    $uniqueName =  md5($file['tmp_name'] . time());
+    $uniqueName = md5($file['tmp_name'] . time());
     $destinationDir = 'uploads';
-    
-    move_uploaded_file($file['tmp_name'], $destinationDir . '/' . $uniqueName .'.' . $ext);
-  
+
+    move_uploaded_file($file['tmp_name'], $destinationDir . '/' . $uniqueName . '.' . $ext);
+
     //
     $values = array('filename' => $filename, 'secretdir' => $uniqueName);
-   
+
     //
-    
     // add the condition for adding file
-    
-    
+
+
     if ($errorList) {
-     
+
         $app->render("/file_addedit.html.twig", array(
             "errorList" => $errorList,
             'isEditing' => ($id != -1),
             'f' => $values));
     } else { // 2. successful submission
-       
         if ($id != -1) {
             DB::update('files', $values, "id=%i", $id);
         } else {
@@ -175,46 +170,41 @@ $app->post('/file/:op(/:id)', function($op, $id = -1) use ($app, $log) {
     'op' => '(edit|add)',
     'id' => '\d+'
 )); // End of add-edit file
-
 ////choose user
 
 $app->get('/file/share/:id', function($id) use ($app) {
-    if (!$_SESSION['user'] ) {
+    if (!$_SESSION['user']) {
         $app->render("access_denied.html.twig");
         return;
     }
-    if (!$_SESSION['user'] ) {
+    if (!$_SESSION['user']) {
         echo 'Access denied';
         return;
     }
     //
     $usersList = DB::query("SELECT * FROM users");
-     $app->render('file_share.html.twig', array('list' => $usersList));
+    $app->render('file_share.html.twig', array('list' => $usersList));
 });
 
 
 $app->post('/file/share/:id', function($id) use ($app) {
     //echo 'In file chooser post';
-if (!$_SESSION['user']) {
+    if (!$_SESSION['user']) {
         $app->render("access_denied.html.twig");
         return;
     }
     $user = $app->request()->post('user');
-    if (!$user ) {
+    if (!$user) {
         $app->render('/not_found.html.twig');
         echo 'In file chooser post';
         return;
     }
-    if (!DB::queryFirstRow('select id from shares where fileid=%i and userid =%i',$id ,$user))
-            
-    { 
-     DB::insert('shares', array('fileid' => $id, 'userid' => $user));
+    if (!DB::queryFirstRow('select id from shares where fileid=%i and userid =%i', $id, $user)) {
+        DB::insert('shares', array('fileid' => $id, 'userid' => $user));
         if (DB::affectedRows() == 0) {
-        $app->render('/not_found.html.twig');
+            $app->render('/not_found.html.twig');
+        }
     }
-    
-    } 
-        $app->render('/file_share_success.html.twig');
-    
+    $app->render('/file_share_success.html.twig');
 });
 
